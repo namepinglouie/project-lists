d110a37b0872a5d73a62a5ad375a48d6b3227870
Angie Munoz <angiepmunoz@gmail.com>
2024-07-15 11:42:28 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Project from "./components/Project"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Project />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Project.jsx
@@ -0,0 +1,12 @@
+const Project = () => {
+    return (
+        <article className="project">
+            <img src="src/assets/deno-icon.png"/>
+            <p className="project_name">Deno</p>
+            <p className="project_description">Test your Deno project</p>
+            <button>Configure</button>
+        </article>
+    )
+};
+
+export default Project;
